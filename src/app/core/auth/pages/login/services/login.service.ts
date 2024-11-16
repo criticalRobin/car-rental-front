a963a26e12ec0cafbc2f19394aa3e26fac1dabe7
criticalRobin <matiastite1@gmail.com>
2024-11-16 10:51:43 -0500

    Implementado servicio logout

--- src/app/core/auth/pages/login/services/login.service.ts
+++ src/app/core/auth/pages/login/services/login.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from 'environments/environment.development';
 import { ILoginRequest, ILoginResponse } from '../models/login.interface';
 import { Observable } from 'rxjs';
+import { AuthService } from '@core/auth/services/auth.service';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,7 @@ import { Observable } from 'rxjs';
 export class LoginService {
   private baseUrl: string = environment.BASE_URL;
   private readonly http: HttpClient = inject(HttpClient);
+  private readonly authSrv: AuthService = inject(AuthService);
 
   login(loginData: ILoginRequest): Observable<ILoginResponse> {
     const loginUrl: string = `${this.baseUrl}/auth/login`;
@@ -19,5 +21,9 @@ export class LoginService {
     );
 
     return response;
+  }
+
+  logout(): void {
+    this.authSrv.removeLoggedUserFromLocalStorage();
   }
 }
