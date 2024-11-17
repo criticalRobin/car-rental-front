9a3fa2ab5d256b6cb14fc6f500432dd1861edee0
criticalRobin <matiastite1@gmail.com>
2024-11-17 13:48:31 -0500

    Eliminados componentes de prueba de rutas

--- src/app/core/auth/auth.routes.ts
+++ src/app/core/auth/auth.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
-import { RoleGuard } from './guards/role.guard';
 
 export const authRoutes: Routes = [
   {
@@ -16,21 +14,5 @@ export const authRoutes: Routes = [
       import('@core/auth/pages/register/register.component').then(
         (c) => c.RegisterComponent
       ),
-  },
-  {
-    path: 'catalog',
-    loadComponent: () =>
-      import('@core/auth/pages/vehicles/vehicles.component').then(
-        (c) => c.VehiclesComponent
-      ),
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'admin',
-    loadComponent: () =>
-      import('@core/auth/pages/admin/admin.component').then(
-        (c) => c.AdminComponent
-      ),
-    canActivate: [AuthGuard],
   },
 ];
