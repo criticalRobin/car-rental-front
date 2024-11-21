a2a610c7ab3cf2624ec6f04dd35f7fda3157e034
criticalRobin <matiastite1@gmail.com>
2024-11-21 10:52:53 -0500

    Implementada página de deatalle vehiculo

--- src/app/features/vehicle-detail/vehicle-detail.component.ts
+++ src/app/features/vehicle-detail/vehicle-detail.component.ts
@@ -2,26 +2,27 @@ import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { ICatalogVehicle } from '@features/catalog/models/catalog-vehicle.interface';
 import { GaleryComponent } from './components/galery/galery.component';
+import { DetailCardComponent } from "./components/detail-card/detail-card.component";
 
 @Component({
   selector: 'app-vehicle-detail',
   standalone: true,
-  imports: [GaleryComponent],
+  imports: [GaleryComponent, DetailCardComponent],
   templateUrl: './vehicle-detail.component.html',
   styleUrl: './vehicle-detail.component.css',
 })
 export class VehicleDetailComponent {
   private readonly router: Router = inject(Router);
 
+  protected vehicle: ICatalogVehicle | null = null;
   protected images: string[] = [];
 
   constructor() {
-    const vehicle: ICatalogVehicle =
+    this.vehicle =
       this.router.getCurrentNavigation()?.extras.state?.['vehicle'];
-    console.log(vehicle);
 
-    if (vehicle) {
-      this.images = this.chargeImagesArray(vehicle);
+    if (this.vehicle) {
+      this.images = this.chargeImagesArray(this.vehicle);
     }
   }
 
