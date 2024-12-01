0fa5e2554de49a3d36d2eb48339c91c8870d10c9
criticalRobin <matiastite1@gmail.com>
2024-12-01 12:29:31 -0500

    Implementado loader para solicitudes al back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,20 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BaseLayoutComponent } from './core/layout/components/base-layout/base-layout.component';
 import { AuthService } from '@core/auth/services/auth.service';
+import { SpinnerComponent } from './shared/components/spinner/spinner.component';
+import { CommonModule } from '@angular/common';
+import { LoadingService } from '@shared/services/loading.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BaseLayoutComponent],
+  imports: [CommonModule, RouterOutlet, BaseLayoutComponent, SpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   private readonly authSrv: AuthService = inject(AuthService);
+  protected readonly loadingSrv: LoadingService = inject(LoadingService);
 
   protected isLogged: boolean = false;
 
--- src/app/core/interceptors/http-request.interceptor.ts
+++ src/app/core/interceptors/http-request.interceptor.ts
@@ -1,17 +1,30 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '@core/auth/services/auth.service';
+import { LoadingService } from '@shared/services/loading.service';
+import { finalize } from 'rxjs';
+
+let totalRequests: number = 0;
 
 export const httpRequestInterceptor: HttpInterceptorFn = (req, next) => {
   const authSrv: AuthService = inject(AuthService);
+  const loadingSrv: LoadingService = inject(LoadingService);
   const { token } = authSrv.getLoggedUserFromLocalStorage();
 
-  if (token) {
-    const modifiedReq = req.clone({
-      headers: req.headers.set('Authorization', `${token}`),
-    });
-    return next(modifiedReq);
-  }
+  totalRequests++;
+  loadingSrv.setLoading(true);
 
-  return next(req);
+  const modifiedReq = token
+    ? req.clone({
+        headers: req.headers.set('Authorization', `${token}`),
+      })
+    : req;
+
+  return next(modifiedReq).pipe(
+    finalize(() => {
+      totalRequests--;
+
+      if (totalRequests === 0) loadingSrv.setLoading(false);
+    })
+  );
 };
