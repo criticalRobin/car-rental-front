6fbcb7819ea29e85051cd3378b276244257cb23a
Edder <enaranjo5489@uta.edu.ec>
2025-01-05 16:15:44 -0500

    d

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  BASE_URL: process.env['NG_APP_BASE_URL'] || 'https://proyecto-alquiler-vehiculos.onrender.com',
+  BASE_URL: 'https://proyecto-alquiler-vehiculos.onrender.com',
 };
