25a75bb11235289d2a702ba915b7fa8b5b5ecd97
davayme <davayme@gmail.com>
2025-01-11 19:14:20 -0500

    Implementar verificación de correo electrónico en los servicios de inicio de sesión y registro

--- src/app/core/auth/pages/register/services/register.service.ts
+++ src/app/core/auth/pages/register/services/register.service.ts
@@ -2,8 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from 'environments/environment.development';
 import { IRegisterRequest } from '../models/register.interface';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { IUser } from '@core/auth/models/user.interface';
+import { auth } from 'firebase-config';
+import { getAuth, sendEmailVerification, signInWithEmailAndPassword } from 'firebase/auth';
 
 @Injectable({
   providedIn: 'root',
@@ -17,6 +19,26 @@ export class RegisterService {
     const response: Observable<IUser> = this.http.post<IUser>(
       registerUrl,
       registerData
+    ).pipe(
+      tap(async (response) => {
+        console.log('Respuesta del backend:', response);
+        
+        try {
+          // Iniciamos sesión con el email y password para obtener el usuario
+          const auth = getAuth();
+          const userCredential = await signInWithEmailAndPassword(
+            auth,
+            registerData.email,
+            registerData.password
+          );
+          
+          // Enviamos el correo de verificación
+          await sendEmailVerification(userCredential.user);
+          console.log('Correo de verificación enviado');
+        } catch (error) {
+          console.error('Error al enviar correo:', error);
+        }
+      })
     );
 
     return response;
